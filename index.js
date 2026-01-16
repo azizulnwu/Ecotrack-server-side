1966aafba2bf0a13bc0ef79617ce43f30d94824f
azizulnwu <azizulnwu@gmail.com>
2026-01-16 16:40:28 +0600

    Vercel error fix

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 3000;
 
 // index.js
-const decoded = Buffer.from(process.env.FIREBASE_SERVICE_KEY, "base64").toString("utf8");
-const serviceAccount = JSON.parse(decoded);
+const decoded = Buffer.from(process.env.FIREBASE_SERVICE_KEY.replace(/\\n/g, '\n'), "base64").toString("utf8");
+const serviceAccount = JSON.parse(decoded).
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-
+J
 // Middlewares
 app.use(express.json());
 app.use(cors({
